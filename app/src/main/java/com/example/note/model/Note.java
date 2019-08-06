4a8fa257c9f812433db344ac5ed532a93a14d862
AbdulrahmanOsama <abdulrahmantabl@gmail.com>
2019-08-06 15:27:55 +0200

    this the full project

---
+++ app/src/main/java/com/example/note/model/Note.java
@@ -0,0 +1,33 @@
+package com.example.note.model;
+
+import io.realm.RealmObject;
+
+public class Note extends RealmObject {
+    private int id;
+    private String noteTitle;
+    private String noteContent;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNoteTitle() {
+        return noteTitle;
+    }
+
+    public void setNoteTitle(String noteTitle) {
+        this.noteTitle = noteTitle;
+    }
+
+    public String getNoteContent() {
+        return noteContent;
+    }
+
+    public void setNoteContent(String noteContent) {
+        this.noteContent = noteContent;
+    }
+}
